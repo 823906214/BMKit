600f567696dba53a6907eae856eef6e2f95c35eb
Yanry <1151555877@qq.com>
2020-12-17 21:03:20 +0800

    1.view方法添加

--- BlackMoomKit/BlackMoomKit/UI/BMView.h
+++ BlackMoomKit/BlackMoomKit/UI/BMView.h
@@ -13,9 +13,31 @@ NS_ASSUME_NONNULL_BEGIN
 @interface BMView : UIView
 
 /**
- 功能计划:
- 1.添加tap的block方法
- ***/
+ 点击事件
+ @param block 点击事件回调
+ */
+- (void)bm_clickEventBlock:(void(^)(UITapGestureRecognizer * gesture))block;
+
+/**
+ 设置某个圆角
+ 
+ @param corners UIRectCornerTopLeft|UIRectCornerTopRight |连接，设置左右
+ @param cornerRadius 圆角角度
+ */
+- (void)pathWithRoundedRect:(UIRectCorner)corners cornerRadius:(CGFloat )cornerRadius;
+
+/**
+ 设置圆角和阴影
+ 
+ @param radius 圆角
+ @param shadowColor 阴影颜色
+ @param shadowRadius 阴影圆角
+ @param shadowOffset 偏移
+ @param shadowOpacity 阴影透明度
+ */
+- (void)setShadowWithcornerRadius:(CGFloat)radius shadowColor:(UIColor *)shadowColor shadowRadius:(CGFloat)shadowRadius shadowOffset:(CGSize)shadowOffset shadowOpacity:(CGFloat)shadowOpacity;
+
+
 
 @end
 
--- BlackMoomKit/BlackMoomKit/UI/BMView.m
+++ BlackMoomKit/BlackMoomKit/UI/BMView.m
@@ -7,15 +7,65 @@
 //
 
 #import "BMView.h"
+typedef void(^ViewTargetBlock)(UITapGestureRecognizer * gesture);
+@interface BMView()
 
+@property (nonatomic,copy) ViewTargetBlock viewTargetBlock;
+
+@end
 @implementation BMView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+#pragma mark - click
+- (void)bm_clickEventBlock:(void (^)(UITapGestureRecognizer * _Nonnull))block{
+    UITapGestureRecognizer *gesture = [[UITapGestureRecognizer alloc] init];
+    gesture.numberOfTapsRequired = 1;
+    [gesture addTarget:self action:@selector(handlerWithViewTarget:)];
+    [self addGestureRecognizer:gesture];
+    self.viewTargetBlock = block;
 }
-*/
+
+- (void)handlerWithViewTarget:(UITapGestureRecognizer *)gesture{
+    if (self.viewTargetBlock){
+        self.viewTargetBlock(gesture);
+    }
+}
+
+#pragma mark - radius 和 shadow
+- (void)pathWithRoundedRect:(UIRectCorner)corners cornerRadius:(CGFloat )cornerRadius{
+    
+    UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:self.bounds byRoundingCorners:corners cornerRadii:CGSizeMake(cornerRadius,cornerRadius)];
+    
+    CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
+    
+    maskLayer.frame = self.bounds;
+    
+    maskLayer.path = maskPath.CGPath;
+    
+    self.layer.mask = maskLayer;
+}
+
+- (void)setShadowWithcornerRadius:(CGFloat)radius shadowColor:(UIColor *)shadowColor shadowRadius:(CGFloat)shadowRadius shadowOffset:(CGSize)shadowOffset shadowOpacity:(CGFloat)shadowOpacity{
+    
+    if (radius) {
+        self.layer.cornerRadius = radius;
+    }
+    
+    self.layer.backgroundColor = self.backgroundColor.CGColor;
+    
+    
+    if (shadowColor) {
+        self.layer.shadowColor = shadowColor.CGColor;
+    }
+    
+    self.layer.shadowOffset = shadowOffset;
+    
+    if (shadowOpacity) {
+        self.layer.shadowOpacity = shadowOpacity;
+    }
+    
+    if (shadowRadius) {
+        self.layer.shadowRadius = shadowRadius;
+    }
+}
 
 @end
